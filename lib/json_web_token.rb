3f36d29d05794c566b27400007034d1aab1fe6c2
Matthew Wolfe <m5wolfe@edu.uwaterloo.ca>
2020-06-14 10:53:21 -0400

    fixed deploy authentication

--- lib/json_web_token.rb
+++ lib/json_web_token.rb
@@ -4,16 +4,11 @@ class JsonWebToken
     def encode(payload, exp = 4.hours.from_now)
       payload[:exp] = exp.to_i
       secret = Rails.application.secrets.secret_key_base || Rails.application.credentials.secret_key_base || ENV['SECRET_KEY_BASE']
-      puts secret
-      puts secret.nil?
-      secret2 = Rails.application.credentials.dig(:secret_key_base)
-      puts secret2
-      puts secret2.nil?
       JWT.encode(payload, secret)
     end
 
     def decode(token)
-      secret = Rails.application.secrets.secret_key_base || Rails.application.credentials.secret_key_base
+      secret = Rails.application.secrets.secret_key_base || Rails.application.credentials.secret_key_base || ENV['SECRET_KEY_BASE']
       body = JWT.decode(token, secret)[0]
       HashWithIndifferentAccess.new body
     rescue
