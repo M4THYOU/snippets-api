243734869933473b6d123c6200dde6b74f4b108c
Matt Wolfe <m5wolfe@uwaterloo.ca>
2020-07-04 13:08:00 -0400

    basic note taking

--- app/controllers/api/v1/lessons_controller.rb
+++ app/controllers/api/v1/lessons_controller.rb
@@ -30,30 +30,76 @@ module Api
         # URole.create({role_type: Rails.configuration.x.u_role_types.lesson_member, uid: uid, group_id: group.id, created_by_uid: uid})
         URole.create({role_type: Rails.configuration.x.u_role_types.lesson_owner, uid: uid, group_id: group.id, created_by_uid: uid})
         # create lessons
-        pages.each do |page|
-          lesson = Lesson.new(page)
-          lesson[:created_by_uid] = uid
-          lesson[:group_id] = group.id
-          lesson[:title] = title
-          lesson[:course] = course
-          if lesson.save
-            response = RenderJson.success 'Saved lesson', {group_id: group.id}
-            status = :ok
-          end
+        if create_pages(pages, group.id, title, course, uid)
+          response = RenderJson.success 'Saved lesson', { group_id: group.id }
+          status = :ok
         end
         render json: response, status: status
       end
 
       def update
+        response = RenderJson.error 'Lesson not updated', {}
+        status = :unprocessable_entity
 
+        uid = @current_user.id
+        group_id = params[:id]
+
+        roles = URole.where(['group_id = ? and uid = ? and role_type = ? and is_revoked = ?', group_id, uid, Rails.configuration.x.u_role_types.lesson_owner, 0])
+        unless roles.empty?
+          title = lesson_update_params[:title]
+          course = lesson_update_params[:course]
+          pages = lesson_update_params[:pages]
+
+          Lesson.where(['group_id = ?', group_id]).delete_all
+          if create_pages(pages, group_id, title, course, uid)
+            response = RenderJson.success 'Updated lesson', { group_id: group_id }
+            status = :ok
+          end
+        end
+
+        render json: response, status: status
       end
 
-      def destroy; end
+      def destroy
+        response = RenderJson.error 'Lesson not deleted', {}
+        status = :unprocessable_entity
+
+        uid = @current_user.id
+        group_id = params[:id]
+
+        roles = URole.where(['group_id = ? and uid = ? and role_type = ? and is_revoked = ?', group_id, uid, Rails.configuration.x.u_role_types.lesson_owner, 0])
+        unless roles.empty?
+          URole.where(['group_id = ?', group_id]).update_all(is_revoked: 1)
+          Lesson.where(['group_id = ?', group_id]).delete_all
+          UGroup.delete(group_id)
+          response = RenderJson.success 'Lessons successfully deleted', { group_id: group_id }
+          status = :ok
+        end
+        render json: response, status: status
+      end
 
       private
 
       def lesson_params
         params.permit(:title, :course, pages: %i[group_order canvas group_id])
+      end
+      
+      def lesson_update_params
+        params.permit(:title, :course, pages: %i[group_order canvas group_id created_at])
+      end
+
+      def create_pages(pages, group_id, title, course, uid)
+        success = false
+        pages.each do |page|
+          lesson = Lesson.new(page)
+          lesson[:created_by_uid] = uid
+          lesson[:group_id] = group_id
+          lesson[:title] = title
+          lesson[:course] = course
+
+          success = true if lesson.save
+        end
+        success
       end
 
     end
